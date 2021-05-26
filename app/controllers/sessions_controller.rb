6960022e732548995691b8a9433b575b66766584
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-05-26 17:14:43 +0200

    Create user registration and login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(name:login_params[:name])
+        if user
+            session[:user_id] = user.id
+            redirect_to '/dashboard'
+        else
+            flash[:login_errors] =  ['invalid credentials']
+            render 'new'
+        end
+    end
+
+    private
+
+    def login_params
+     params.require(:login).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  get '/users', to: 'users#new'
+  get '/sessions', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+  post '/users', to: 'users#create'
+  get '/dashboard', to: 'activities#index'
 end
